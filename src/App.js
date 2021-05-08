b03fd0d37ee5e82100a627b5e06601ff285f8ba9
ashinde3 <ashinde3@binghamton.edu>
2021-05-08 18:19:24 -0400

    Created sidebar, added styling and wrapped in App component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import {useSelector} from 'react-redux';
 import Login from './Views/Login/login'
+import {BrowserRouter as Router} from 'react-router-dom';
+import Sidebar from './Views/Sidebar/sidebar';
 //import './App.css';
 
 
 function App() {
+  const {auth} = useSelector(state => state);
   return (
-    <div>
-      <Login />
-    </div>
+    <Router>
+      {!auth.LoggedIn ? <Login /> : 
+        <div>
+          <Sidebar />
+        </div>
+      }
+    </Router>
   );
 }
 
