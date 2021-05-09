afa3edc859b6857eb474d2fae09f44429b8a645d
ashinde3 <ashinde3@binghamton.edu>
2021-05-09 17:09:21 -0400

    Updated views components and pages

--- src/Views/Pages/viewBugs.jsx
+++ src/Views/Pages/viewBugs.jsx
@@ -1,9 +1,14 @@
-import React, {useEffect} from 'react';
+import React, {useEffect,useState} from 'react';
 import {useDispatch,useSelector} from 'react-redux';
-import {getBugs} from '../../Controllers/Redux/bugSlice';
+import bugSlice, {getBugs} from '../../Controllers/Redux/bugSlice';
 import BugCard from '../Components/Bug Card/bugCard';
+import BugView from '../Components/Bug view/bugView';
 
 export default() => {
+    const[DISPLAY_BUG,SET_DISPLAY_BUG] = useState({
+        name: "",
+        isDisplayed: false
+    });
     const dispatch = useDispatch();
     const {bugs} = useSelector(state => state);
 
@@ -15,7 +20,10 @@ export default() => {
     }, [bugs.length < 1]) 
 
     function BugClicked(name) {
-
+        SET_DISPLAY_BUG({
+            isDisplayed: !DISPLAY_BUG.isDisplayed,
+            name: name
+        });
     }
 
     return(
@@ -23,6 +31,7 @@ export default() => {
             {bugs.map((bug,key) => (
                 <BugCard key={key} bug={bug} clicked={BugClicked} />
             ))}
+            {DISPLAY_BUG.isDisplayed && <BugView clicked={BugClicked} bug={bugs.filter((bug) => bug.name === DISPLAY_BUG.name)[0]} />}
         </div>
     );
 }
